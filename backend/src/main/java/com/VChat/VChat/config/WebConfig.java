c42c07066a57ad67817ac765e54383f72a10e451
Kabir Singh <kabir.singh66564@example.com>
2025-11-05 20:38:59 +0530

    Add frontend and backend - ready for deployment

--- backend/src/main/java/com/VChat/VChat/config/WebConfig.java
+++ backend/src/main/java/com/VChat/VChat/config/WebConfig.java
@@ -16,8 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000",  // React dev\n" +
-                                "https://zoomclone-frontend.onrender.com")
+                        .allowedOrigins("https://vchatpage.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- backend/src/main/java/com/VChat/VChat/config/WebSocketConfig.java
+++ backend/src/main/java/com/VChat/VChat/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new SignalingHandler(),"/ws")
-                .setAllowedOrigins("http://localhost:3000", "https://zoomclone-frontend.onrender.com")
+                .setAllowedOrigins("https://vchatpage.onrender.com")
                 .withSockJS();
     }
 }
