b0c0227c2d9ec04dc6bb3325c803f84359fd01a8
Kabir Singh <kabir.singh66564@example.com>
2025-11-06 14:30:27 +0530

    Add frontend and backend - ready for deployment

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1269,16 +1269,24 @@
 
 
 
-import React, { useEffect, useRef, useState } from "react";
+
 import SockJS from "sockjs-client";
 import { over } from "stompjs";
-import { IconButton, TextField, Button, Badge } from "@mui/material";
-import VideocamIcon from "@mui/icons-material/Videocam";
-import VideocamOffIcon from "@mui/icons-material/VideocamOff";
-import CallEndIcon from "@mui/icons-material/CallEnd";
-import ScreenShareIcon from "@mui/icons-material/ScreenShare";
-import ChatIcon from "@mui/icons-material/Chat";
+import React, { useEffect, useRef, useState } from 'react'
+import io from "socket.io-client";
+import { Badge, IconButton, TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import VideocamOffIcon from '@mui/icons-material/VideocamOff'
 import styles from "../styles/videoComponent.module.css";
+import CallEndIcon from '@mui/icons-material/CallEnd'
+import MicIcon from '@mui/icons-material/Mic'
+import MicOffIcon from '@mui/icons-material/MicOff'
+import ScreenShareIcon from '@mui/icons-material/ScreenShare';
+import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
+import ChatIcon from '@mui/icons-material/Chat'
+import server from '../environment';
+
 
 const server_url = "https://vchat-rp52.onrender.com"; // your Spring Boot backend
 let stompClient = null;
