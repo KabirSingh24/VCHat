1c4d794dfbf93ee6de0bbfbbe52950651b3b3e5d
Kabir Singh <kabir.singh66564@example.com>
2025-11-06 21:38:01 +0530

    Ready For Deployment

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -16,49 +16,49 @@ import { AuthContext } from '../contexts/AuthContext';
 import { Snackbar } from '@mui/material';
 
 
-const defaultTheme = createTheme();
 
 export default function Authentication() {
 
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [error, setError] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const defaultTheme = createTheme();
+
+  const [username, setUsername] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [name, setName] = React.useState();
+  const [error, setError] = React.useState();
+  const [message, setMessage] = React.useState();
 
 
   const [formState, setFormState] = React.useState(0);
-  const [open, setOpen] = React.useState(false);
+
+  const [open, setOpen] = React.useState(false)
 
 
-  const { handlerRegister, handleLogin } = React.useContext(AuthContext);
+  const { handleRegister, handleLogin } = React.useContext(AuthContext);
+
 
   let handleAuth = async () => {
     try {
       if (formState === 0) {
-        let result = await handleLogin(username, password);
-        console.log(result);
-        setMessage(result);
-        setError("");
+
+        let result = await handleLogin(username, password)
+
+
       }
       if (formState === 1) {
-        let result = await handlerRegister(name, username, password);
+        let result = await handleRegister(name, username, password);
+        console.log(result);
+        setUsername("");
         setMessage(result);
         setOpen(true);
-        setFormState(0);
-        setPassword("");
-        setUsername("");
+        setError("")
+        setFormState(0)
+        setPassword("")
       }
     } catch (err) {
+
       console.log(err);
-      if (err.response && err.response.data && err.response.data.error) {
-        setError(err.response.data.error);
-      } else if (typeof err === "string") {
-        setError(err);
-      } else {
-        setError("Something went wrong"); // fallback
-      }
-      setMessage(""); // clear any previous success message
+      let message = (err.response.data.message);
+      setError(message);
     }
   }
 
@@ -84,79 +84,96 @@ export default function Authentication() {
           boxShadow: "2px 0px 8px rgba(0,0,0,0.1)",
         }}
       >
-        <Avatar sx={{ m: 1,mt:5, bgcolor: 'secondary.main' }}><LockOutlinedIcon />
+        <Avatar sx={{ m: 1, mt: 5, bgcolor: 'secondary.main' }}><LockOutlinedIcon />
         </Avatar>
 
 
-          <div>
-            <Button variant={formState === 0 ? "contained" : ""} onClick={() => { setFormState(0) }}>
-                                Sign In
-            </Button>
-            <Button variant={formState === 1 ? "contained" : ""} onClick={() => { setFormState(1) }}>
-                                Sign Up
-            </Button>
-          </div>
+        <div>
+          <Button variant={formState === 0 ? "contained" : ""} onClick={() => { setFormState(0) }}>
+            Sign In
+          </Button>
+          <Button variant={formState === 1 ? "contained" : ""} onClick={() => { setFormState(1) }}>
+            Sign Up
+          </Button>
+        </div>
 
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-              {formState === 1 ? <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="username"
-                                label="Full Name"
-                                name="username"
-                                value={name}
-                                autoFocus
-                                autoComplete="off"
-                                onChange={(e) => setName(e.target.value)}
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          {formState === 1 ? <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Full Name"
+            name="username"
+            value={name}
+            autoFocus
+            onChange={(e) => setName(e.target.value)}
           /> : <></>}
 
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="username"
-                                label="Username"
-                                name="username"
-                                value={username}
-                                autoFocus
-                                autoComplete="off"
-                                onChange={(e) => setUsername(e.target.value)}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            value={username}
+            autoFocus
+            onChange={(e) => setUsername(e.target.value)}
 
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                value={password}
-                                type="password"
-                                autoComplete="new-password" 
-                                onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
 
-                                id="password"
-                            />
+            id="password"
+          />
 
-                            <p style={{color:"red"}}>{error}</p>
+          <p style={{ color: "red" }}>{error}</p>
 
-                            <Button
-                                type="button"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                                onClick={handleAuth}
-                            >
-                                {formState === 0 ? "Login " : "Register"}
-                            </Button>
-      </Box>
+
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleAuth}
+          >
+            {formState === 0 ? "Login " : "Register"}
+          </Button>
+
+        </Box>
       </Box>
       <Snackbar
 
-                open={open}
-                autoHideDuration={4000}
-                message={message}
-            />
+        open={open}
+        autoHideDuration={4000}
+        message={message}
+      />
     </Box>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
